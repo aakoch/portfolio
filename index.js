bb08df5bdf6072289243db32dfe261017be79b98
Adam Koch <adam@dogtired.org>
2021-01-09 11:55:27 -0600

    first use of pug

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import { } from 'bootstrap';
-import {} from 'nunjucks';
+// import {} from 'nunjucks';
 
-customElements.define('test-template',
-  class extends HTMLElement {
-    constructor() {
-      super();
-      const template = document
-        .getElementById('test-template-template')
-        .content;
-      const shadowRoot = this.attachShadow({mode: 'open'})
-        .appendChild(template.cloneNode(true));
-    }
-  }
-);
+// customElements.define('test-template',
+//   class extends HTMLElement {
+//     constructor() {
+//       super();
+//       const template = document
+//         .getElementById('test-template-template')
+//         .content;
+//       const shadowRoot = this.attachShadow({mode: 'open'})
+//         .appendChild(template.cloneNode(true));
+//     }
+//   }
+// );
 
 // customElements.define('card-template',
 //   class extends HTMLElement {
